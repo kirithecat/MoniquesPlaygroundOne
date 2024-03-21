28ee7f14e78e245691150b28525974cebe50713e
nique-noc <36695580+kirithecat@users.noreply.github.com>
2024-03-21 19:28:51 +1300

    Even or odd solution (#1)
    * renamed index.js and test.js and created EvenOrOdd problem
    * actual solution working
    * formatting issues resolved and test name changed
    * renamed files to use camel case
    * renamed test files

---
+++ EvenOrOdd.js
@@ -0,0 +1,6 @@
+export function evenOrOdd(number) {
+    if (number % 2 !== 0) {
+        return "Odd";
+    }
+    return "Even";
+}
---
+++ test/evenOrOdd.test.js
@@ -0,0 +1,20 @@
+import { evenOrOdd } from "../evenOrOdd.js";
+import { equal } from 'assert';
+
+it('Even number test (2)', () => {
+    const result = evenOrOdd(2);
+    const expectedResult = "Even";
+    equal(result, expectedResult);
+});
+
+it('Even number test (0)', () => {
+    const result = evenOrOdd(0);
+    const expectedResult = "Even";
+    equal(result, expectedResult);
+});
+
+it('Odd number test (3)', () => {
+    const result = evenOrOdd(3);
+    const expectedResult = "Odd";
+    equal(result, expectedResult);
+});
