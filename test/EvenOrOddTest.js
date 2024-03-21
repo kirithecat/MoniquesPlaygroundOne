1c3d83992b2d69205f44a6d311939127b301069e
YoungMoniD <moniquedignadice@gmail.com>
2024-03-21 19:07:04 +1300

    formating issues resolved and test name changed

--- test/EvenOrOddTest.js
+++ test/EvenOrOddTest.js
@@ -1,19 +1,19 @@
 import { evenOrOdd } from "../EvenOrOdd.js";
 import { equal } from 'assert';
 
-it('Even number test (2)', function() {
+it('Even number test (2)', () => {
     const result = evenOrOdd(2);
     const expectedResult = "Even";
     equal(result, expectedResult);
 });
 
-it('Even number test (0)', function() {
+it('Even number test (0)', () => {
     const result = evenOrOdd(0);
     const expectedResult = "Even";
     equal(result, expectedResult);
 });
 
-it('Even number test (3)', function() {
+it('Odd number test (3)', () => {
     const result = evenOrOdd(3);
     const expectedResult = "Odd";
     equal(result, expectedResult);
